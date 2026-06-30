6372bf3d2163e0c9781c683961d06dc48e27bd6c
Roshan29904 <roshan.rai29904@gmail.com>
2026-07-01 04:23:14 +0530

    profile creation done

---
+++ controllers/Profile.js
@@ -0,0 +1,44 @@
+const Profile = require("../models/Profile")
+const User = require("../models/User");
+
+
+// update profile as profile is made in middleware
+exports.updateProfile = async (req, res) => {
+    try {
+        //get data
+        const { dateOfBirth="", about="", contactNumber, gender } = req.body;
+        //get userId
+        const id = req.user.id;
+        //validation
+        if(!contactNumber || !gender || !id) {
+            return res.status(400).json({
+                success: false,
+                message: 'All fields are required',
+            });
+        }
+        //find profile
+        const userDetails = await User.findById(id);
+        const profileId = userDetails.additionalDetails;
+        const profileDetails = await Profile.findById(profileId);
+
+        //update profile
+        profileDetails.dateOfBirth = dateOfBirth;
+        profileDetails.about = about;
+        profileDetails.gender = gender;
+        profileDetails.contactNumber = contactNumber;
+        await profileDetails.save();
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: "profile Updated successfully", 
+            profileDetails,
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+            
+        })
+    }
+}
